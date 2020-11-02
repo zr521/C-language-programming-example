be63bf749d8e963c11361fede36dab9a2c5d6e02
古米 <57127364+zr521@users.noreply.github.com>
2020-11-02 22:14:41 +0800

    scanf()、printf()函数返回值

--- eg02.c
+++ eg02.c
@@ -3,7 +3,7 @@ int main(void)
 {
     printf("%d\n", printf("abcd\n"));//printf()返回值为整形，输出字符的个数
     int x, y;
-    printf("%d\n", scanf("%d%d", &x, &y));//scanf()返回按照正确格式输入的变量的个数
+    printf("%d\n", scanf("%d%d", &x, &y));// scanf()返回按照正确格式输入的变量的个数
     return 0;
 
 }
